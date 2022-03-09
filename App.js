564087648277975920777cf072b1d0d31a165249
Alexandre <chorizosurffista@gmail.com>
2022-03-09 13:34:44 +0100

    Debut app mobile, liaison get avec api fait

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Component} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            data:[]
+        }
+    }
+    componentDidMount(){
+      fetch(`http://localhost:4000/inscription/countriesInscriptionForm`)
+      .then(response => response.json())
+      .then(data => this.setState({data: [data]}));
+  } render(){
+      console.log(this.state.data);
+      const textJsx= this.state.data.map((countries, key)=>(
+          countries.map((country, key)=>(
+            <View>
+                <Text>{country.countryName}</Text>
+            </View>
+        ))
+    ))
+      console.log(textJsx);
+      return(
+        <View>
+            <Text>Hello§</Text>
+                {textJsx}
+        </View>
+      )
+  }
 }
 
 const styles = StyleSheet.create({
